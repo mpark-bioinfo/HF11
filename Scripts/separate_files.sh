d327d6ed6bb4c2fde3dff8ae68f8ec7395a89df2
Meeyoung Park <mparkbio@med.umich.edu>
2016-04-14 09:53:52 -0400

    done!

---
+++ Scripts/separate_files.sh
@@ -0,0 +1,3 @@
+Rscript 7_Grouping_by_Strains.R ../Normalization/SCN_Final.csv ../Normalization/Final_SCN/
+Rscript 7_Grouping_by_Strains.R ../Normalization/Liver_Final.csv ../Normalization/Final_Liver/
+Rscript 7_Grouping_by_Strains.R ../Normalization/Plasma_Final.csv ../Normalization/Final_Plasma/
