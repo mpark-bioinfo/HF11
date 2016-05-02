38fa8d12d5e3496a9353681fcfe8ba01fe9a8da4
MEEYOUNG PARK <Meeyoung@MEEYOUNGs-MacBook-Air.local>
2016-05-02 19:52:50 -0400

    all fc analysis by strain

--- Scripts/corr-1.sh
+++
@@ -1,6 +0,0 @@
-Rscript CorrAnalysis.R ../Normalization/Final_Plasma/Plasma_WT_BKS.csv ../CorrelationAnalysis/
-Rscript CorrAnalysis.R ../Normalization/Final_Plasma/Plasma_WT_BL6.csv ../CorrelationAnalysis/
-Rscript CorrAnalysis.R ../Normalization/Final_Plasma/Plasma_WT_BTBR.csv ../CorrelationAnalysis/
-Rscript CorrAnalysis.R ../Normalization/Final_Plasma/Plasma_db_BKS.csv ../CorrelationAnalysis/
-Rscript CorrAnalysis.R ../Normalization/Final_Plasma/Plasma_db_BL6.csv ../CorrelationAnalysis/
-Rscript CorrAnalysis.R ../Normalization/Final_Plasma/Plasma_db_BTBR.csv ../CorrelationAnalysis/
